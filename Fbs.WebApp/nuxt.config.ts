3861a0422f58ebeb54c5b9385891bfa261368b29
Qin Guan <qinguan20040914@gmail.com>
2025-04-27 15:35:17 +0800

    Add tracking

--- Fbs.WebApp/nuxt.config.ts
+++ Fbs.WebApp/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
@@ -13,6 +12,23 @@ export default defineNuxtConfig({
   ],
   ssr: false,
   devtools: { enabled: true },
+
+  app: {
+    head: {
+      script: [
+        {
+          innerHTML: `
+              (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ra29xtmq6f");
+          `,
+          type: 'text/javascript',
+        },
+      ],
+    },
+  },
 
   css: ['~/assets/css/main.css'],
 
