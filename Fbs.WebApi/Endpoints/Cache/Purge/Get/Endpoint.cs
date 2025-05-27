09212842faaedf7398df4fc9a6510c1e63cd3172
Qin Guan <qinguan20040914@gmail.com>
2025-05-27 14:29:46 +0800

    Cache purge endpoint

---
+++ Fbs.WebApi/Endpoints/Cache/Purge/Get/Endpoint.cs
@@ -0,0 +1,19 @@
+using FastEndpoints;
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Fbs.WebApi.Endpoints.Cache.Purge.Get;
+
+public class Endpoint(HybridCache cache) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/Cache/Purge");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await cache.RemoveAsync(["Facilities", "Bookings", "Nominal Roll", "Users"], ct);
+        await SendOkAsync(ct);
+    }
+}
