685149d0915f4b6b7df904b8e1312371ff048241
Qin Guan <qinguan20040914@gmail.com>
2026-06-07 15:33:29 +0800

    Update

--- Fbs.WebApp/middleware/admin.ts
+++
@@ -1,9 +0,0 @@
-import { defineRouteMiddleware, navigateTo } from '#app'
-
-export default defineRouteMiddleware(async (to, from) => {
-  const { data: me } = await useMe()
-
-  if (!me.value?.isAdmin) {
-    return navigateTo('/booking')
-  }
-})
